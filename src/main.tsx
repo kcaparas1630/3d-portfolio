4b6f607a7512434a7de5125e09394cd7abe79fea
Kent Caparas <kcaparas1630@gmail.com>
2025-10-31 22:08:50 -0700

    added web vitals to check performance

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { onCLS, onFCP, onLCP, onTTFB, onINP } from 'web-vitals'
+
+// Monitor Web Vitals
+onCLS(console.log)
+onFCP(console.log)
+onLCP(console.log)
+onTTFB(console.log)
+onINP(console.log)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
