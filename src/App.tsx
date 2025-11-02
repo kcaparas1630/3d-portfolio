94401e5428bf81042ade4b8297c89d177bc79bed
Kent Caparas <kcaparas1630@gmail.com>
2025-11-01 20:56:06 -0700

    Reverted back to previous setup and removed loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-// import { Canvas } from '@react-three/fiber'
-// import { OrbitControls } from '@react-three/drei'
-// import { Suspense } from 'react'
-// import Model from './Models/Character/Model'
-import LoadingScreen from './Common/View/LoadingScreen'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { Suspense } from 'react'
+import Model from './Models/Character/Model'
+
 
 const App = () => {
   return (
-    <LoadingScreen />
-    // <div style={{ width: '100vw', height: '100vh' }}>
-    //   <Canvas camera={{ position: [0, 1, 5], fov: 50 }}>
-    //     <ambientLight intensity={0.5} />
-    //     <directionalLight position={[5, 5, 5]} intensity={0.8} />
-    //     <Suspense fallback={null}>
-    //       <Model />
-    //     </Suspense>
-    //     <OrbitControls />
-    //     <gridHelper args={[10, 10]} />
-    //   </Canvas>
-    // </div>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas camera={{ position: [0, 1, 5], fov: 50 }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 5, 5]} intensity={0.8} />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+        <OrbitControls />
+        <gridHelper args={[10, 10]} />
+      </Canvas>
+    </div>
   )
 }
 
