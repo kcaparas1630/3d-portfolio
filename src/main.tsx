c993258acd06831348a971a96c5b072f78f88467
Kent Caparas <kcaparas1630@gmail.com>
2025-11-01 20:56:18 -0700

    added routeTree for main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 import './index.css'
-import App from './App.tsx'
+import { routeTree } from './routeTree.gen'
 import { onCLS, onFCP, onLCP, onTTFB, onINP } from 'web-vitals'
 
 // Monitor Web Vitals
@@ -11,8 +12,18 @@ onLCP(console.log)
 onTTFB(console.log)
 onINP(console.log)
 
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
