9ec85ef7230daf62ae17591146b85f9305bd7e45
Kent Caparas <kcaparas1630@gmail.com>
2025-11-09 14:26:05 -0800

    refactored to follow the rigdbody of the character

---
+++ src/utils/CameraController.ts
@@ -0,0 +1,26 @@
+import { useFrame, useThree } from "@react-three/fiber";
+import { Group, Vector3 } from "three";
+
+const CameraController = ({ targetRef }: { targetRef: React.RefObject<Group | null> }) => {
+  const { camera } = useThree();
+
+  useFrame(() => {
+    if (targetRef.current) {
+      // Get world position of the group (which includes RigidBody's position)
+      const worldPosition = new Vector3();
+      targetRef.current.getWorldPosition(worldPosition);
+
+      // Camera follows character horizontally at waist height
+      camera.position.x = worldPosition.x;
+      camera.position.y = -1; // Fixed at waist height (character base is at -2, waist at ~0)
+      camera.position.z = worldPosition.z + 5;
+
+      // Look at the waist position
+      camera.lookAt(worldPosition.x, 0, worldPosition.z);
+    }
+  });
+
+  return null;
+};
+
+export default CameraController;
